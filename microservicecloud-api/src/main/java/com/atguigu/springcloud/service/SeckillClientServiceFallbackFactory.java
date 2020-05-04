a0176a8584a605248f9d0165929fb9b1f801386b
liujun <1980621312@qq.com>
2020-05-04 19:52:57 +0800

    first commit

---
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/service/SeckillClientServiceFallbackFactory.java
@@ -0,0 +1,30 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.Utils.CodeMsg;
+import com.atguigu.springcloud.Utils.Result;
+import com.atguigu.springcloud.entities.MiaoShaMessage;
+import feign.hystrix.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: liujun
+ * \* Date: 2019/8/12
+ * \* Time: 15:00
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+@Component
+public class SeckillClientServiceFallbackFactory implements FallbackFactory<SeckillClientService> {
+
+    @Override
+    public SeckillClientService create(Throwable throwable) {
+        return new SeckillClientService() {
+            @Override
+            public Result<String> doMiaosha(MiaoShaMessage miaoShaMessage) {
+                return Result.error(CodeMsg.stop);
+            }
+        };
+    }
+}
